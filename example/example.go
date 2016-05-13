35b6931aeb4647c697e7a367cb9e37f1229c4895
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-05-13 12:24:42 +0900

    Add example

---
+++ example/example.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zetamatta/pipeline"
+)
+
+func main() {
+	fmt.Println("Start")
+	pipeline.Run(func(in, out chan interface{}) {
+		for i := 0; i < 10; i++ {
+			out <- i
+		}
+	}, func(in, out chan interface{}) {
+		for value := range in {
+			out <- interface{}(value.(int) + 1)
+		}
+	}, func(in, out chan interface{}) {
+		for value := range in {
+			fmt.Printf("%d\n", value.(int))
+		}
+	})
+	fmt.Println("Done.")
+}
